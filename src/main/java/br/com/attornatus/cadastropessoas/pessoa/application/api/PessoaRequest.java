0f5c11ed9d11434161916b68619289bbe1ddb73c
Marily Moreira <marilysantosmoreira92@gmail.com>
2023-10-03 16:45:44 -0300

    implementa request e response

--- src/main/java/br/com/attornatus/cadastropessoas/pessoa/application/api/PessoaRequest.java
+++ src/main/java/br/com/attornatus/cadastropessoas/pessoa/application/api/PessoaRequest.java
@@ -1,5 +1,19 @@
 package br.com.attornatus.cadastropessoas.pessoa.application.api;
 
-public class PessoaRequest {
+import java.time.LocalDate;
 
+import br.com.attornatus.cadastropessoas.pessoa.domain.Endereco;
+import jakarta.persistence.Embedded;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Value;
+
+@Value
+public class PessoaRequest {
+	@NotBlank
+	private String nomeCompleto;
+	@NotNull
+	private LocalDate dataNascimento;
+	@Embedded
+	private Endereco endereco;
 }
--- src/main/java/br/com/attornatus/cadastropessoas/pessoa/application/api/PessoaResponse.java
+++ src/main/java/br/com/attornatus/cadastropessoas/pessoa/application/api/PessoaResponse.java
@@ -1,5 +1,10 @@
 package br.com.attornatus.cadastropessoas.pessoa.application.api;
 
-public class PessoaResponse {
+import java.util.UUID;
 
+import lombok.Value;
+
+@Value
+public class PessoaResponse {
+	private UUID idCliente;
 }
