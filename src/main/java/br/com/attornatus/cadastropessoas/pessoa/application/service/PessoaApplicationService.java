be89b7613f0862e914da0811bfd76a5a0069f693
Marily Moreira <marilysantosmoreira92@gmail.com>
2023-10-03 21:01:03 -0300

    implementa application service

---
+++ src/main/java/br/com/attornatus/cadastropessoas/pessoa/application/service/PessoaApplicationService.java
@@ -0,0 +1,22 @@
+package br.com.attornatus.cadastropessoas.pessoa.application.service;
+
+import org.springframework.stereotype.Service;
+
+import br.com.attornatus.cadastropessoas.pessoa.application.api.PessoaRequest;
+import br.com.attornatus.cadastropessoas.pessoa.application.api.PessoaResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+
+@Service
+@Log4j2
+@RequiredArgsConstructor
+public class PessoaApplicationService implements PessoaService {
+
+	@Override
+	public PessoaResponse criaPessoa(PessoaRequest pessoaRequest) {
+		log.info("[inicia] PessoaApplicationService - criaPessoa");
+		log.info("[finaliza] PessoaApplicationService - criaPessoa");
+		return null;
+	}
+
+}
